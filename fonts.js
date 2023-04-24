750e73a0085220034012cccddf7252e52d0bab89
olawistedt <43708280+olawistedt@users.noreply.github.com>
2023-04-24 19:02:49 +0000

    Deploying to gh-pages from master @ 85446dd437e1aa318758c941266ef8ccb982fd43 🚀

--- fonts.js
+++ fonts.js
@@ -171,7 +171,7 @@ Module['FS_createPath']("/resources", "fonts", true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}], "remote_package_size": 145348, "package_uuid": "c6362cd9-435e-47f2-bd71-6f155c1b56a6"});
+   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}], "remote_package_size": 145348, "package_uuid": "4ef96f5d-4207-4057-a1e7-cda073498e80"});
   
   })();
   
